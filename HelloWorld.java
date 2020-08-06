2af2cf7a6d9a9e2e4ab9be0abed1fca0de8bd30a
penguin12310418 <68837034+penguin12310418@users.noreply.github.com>
2020-08-06 16:49:45 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,9 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!dfgdfgdfgdfgdfgfgdfgdfgdfdfgdfdgddfg");
 	System.out.println("Hello World!123");
 	System.out.println("testOne");
-	    System.out.println("testtwo");
     }
 
 
